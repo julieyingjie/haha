f06654379f31fb054ab5c1a5fb6228749670d2ac
Juile <julieyingjie@gmail.com>
2021-09-22 23:26:33 -0400

    final ver

--- src/demo.java
+++ src/demo.java
@@ -2,5 +2,6 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("Hello Juila");
         System.out.println("LOL success");
+        System.out.println("finally");
     }
 }
